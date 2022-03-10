5f0d30fe4dffc4b10938c23a31b7759edfbd3d51
ann-wangui <wanguia822@gmail.com>
2022-03-10 11:11:16 +0300

    add alert message to test js

---
+++ script.js
@@ -0,0 +1,16 @@
+var maleName=["Kwasi","kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+
+function akanName(){
+var dateOfBirth =document.getElementById("dateofbirth");
+var month =document.getElementById("month");
+var year =document.getElementById("year");
+
+
+
+}
+
+
+
+
